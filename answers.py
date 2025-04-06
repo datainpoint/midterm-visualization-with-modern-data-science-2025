326feb7c684f99512acf4e849cdc0a1cbfc2afed
yaojenkuo <tonykuoyj@gmail.com>
2025-04-06 22:30:16 +0800

    first commit

---
+++ answers.py
@@ -0,0 +1,139 @@
+answer_01 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_02 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_03 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_04 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_05 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_06 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_07 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_08 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_09 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_10 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_11 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_12 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_13 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_14 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_15 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_16 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_17 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_18 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_19 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
+
+answer_20 =\
+"""
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+"""
